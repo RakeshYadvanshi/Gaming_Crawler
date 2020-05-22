1204103ad26b57041af515744aa66f0d3147fd88
DESKTOP-HM9O5MN\Jay Kapoor <jatinkapoor995@gmail.com>
2020-05-22 23:25:03 +0530

    no message

--- game_crawler_url1.php
+++ game_crawler_url1.php
@@ -2,9 +2,7 @@
 /*
 Author: Rakesh
 */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 include( 'Scrapper/simple_html_dom.php'); 
 include('DatabaseLayer/Conn.php');
 include('Model/Option.php');
